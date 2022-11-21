5bb383df1adc71c69f26388427b965acc520e783
alumne <amontero@iesebre.com>
2022-11-21 19:10:12 +0100

    vue cli

---
+++ comptador_vuecli/src/components/counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="decrement">Disminuir</button>
+    <button @click="increment">Aumentar</button>
+  </div>
+
+</template>
+
+<script>
+
+export default  {
+  data (){
+    return{
+    counter: 0
+  }
+
+  },
+  methods:{
+    increment(){
+      this.counter++
+    },
+    decrement(){
+      this.counter--
+    }
+  }
+}
+</script>
